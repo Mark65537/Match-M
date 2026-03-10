79ea3e62eb5eac8b5f1c17f39b4acfaee32584ee
Mark6 <grigorash-mark@mail.ru>
2026-03-10 20:33:44 +0700

    добавил игровой экран с полем, очками и таймером

--- Match-M/ViewModel/MainWindowViewModel.cs
+++ Match-M/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Match_M.ViewModel;
 
@@ -9,9 +11,30 @@ public class MainWindowViewModel : ObservableObject
     private bool _isMainMenuVisible = true;
     private bool _isGameScreenVisible;
 
+    private int _score;
+    private int _timeLeftSeconds;
+    private readonly DispatcherTimer _timer;
+
+    /// <summary>
+    /// Размер поля в клетках
+    /// </summary>
+    private const int BOARD_SIZE = 8;
+
+    public ObservableCollection<int> Cells { get; } = [];
+
     public MainWindowViewModel()
     {
         PlayCommand = new RelayCommand(OnPlay);
+
+        // Настройка таймера (тиков в секунду)
+        _timer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(1)
+        };
+        _timer.Tick += Timer_Tick;
+
+        InitBoard();
+        ResetGameState();
     }
 
     public Visibility MainMenuVisibility =>
@@ -20,11 +43,55 @@ public class MainWindowViewModel : ObservableObject
     public Visibility GameScreenVisibility =>
         _isGameScreenVisible ? Visibility.Visible : Visibility.Collapsed;
 
+    public int Score
+    {
+        get => _score;
+        set => SetProperty(ref _score, value);
+    }
+
+    public string TimeText =>
+        $"{_timeLeftSeconds / 60:00}:{_timeLeftSeconds % 60:00}";
+
     public RelayCommand PlayCommand { get; }
 
     private void OnPlay()
     {
         _isMainMenuVisible = false;
         _isGameScreenVisible = true;
+
+        OnPropertyChanged(nameof(MainMenuVisibility));
+        OnPropertyChanged(nameof(GameScreenVisibility));
+
+        ResetGameState();
+        _timer.Start();
+    }
+
+    private void InitBoard()
+    {
+        Cells.Clear();
+
+        for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++)
+        {
+            Cells.Add(i);
+        }
+    }
+
+    private void ResetGameState()
+    {
+        Score = 0;
+        _timeLeftSeconds = 60;
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+        if (_timeLeftSeconds > 0)
+        {
+            _timeLeftSeconds--;
+            OnPropertyChanged(nameof(TimeText));
+        }
+        else
+        {
+            _timer.Stop();
+        }
     }
 }
