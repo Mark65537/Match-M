c1309da3b36391e23c905c5682ee492c0c73ff37
Mark6 <grigorash-mark@mail.ru>
2026-03-10 23:48:46 +0700

    теперь каждый экран отдельный View это облегчает дизайнерскую работу

---
+++ Match-M/View/GameView.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows.Controls;
+
+namespace Match_M.View
+{
+    public partial class GameView : UserControl
+    {
+        public GameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
---
+++ Match-M/View/MenuView.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows.Controls;
+
+namespace Match_M.View
+{
+    public partial class MenuView : UserControl
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
