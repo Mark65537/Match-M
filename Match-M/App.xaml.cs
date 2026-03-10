42061b7d633d0ced3a4b3e8fe621bdb47b680f02
Mark6 <grigorash-mark@mail.ru>
2026-03-10 18:53:31 +0700

    Добавьте файлы проекта.

---
+++ Match-M/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Match_M
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
