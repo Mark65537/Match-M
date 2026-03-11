665a96e74d06d2831360b310935b1176393c485d
Mark6 <grigorash-mark@mail.ru>
2026-03-11 18:36:07 +0700

    добавил возможность отладки отдельных состояний

--- Match-M/ViewModel/MainWindowViewModel.cs
+++ Match-M/ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Match_M.Model;
 
 namespace Match_M.ViewModel;
@@ -10,12 +11,22 @@ public class MainWindowViewModel : ObservableObject
     {
         GameStateService = new GameStateService
         {
+#if DEBUG
+            CurrentState = GameState.InGame
+#else
             CurrentState = GameState.Menu
+#endif
         };
 
         MenuVM = new MenuViewModel(GameStateService);
         GameVM = new GameViewModel(GameStateService);
         GameOverVM = new GameOverViewModel(GameStateService);
+
+#if DEBUG
+        ToMenuCommand = new RelayCommand(() => GameStateService.CurrentState = GameState.Menu);
+        ToInGameCommand = new RelayCommand(() => GameStateService.CurrentState = GameState.InGame);
+        ToGameOverCommand = new RelayCommand(() => GameStateService.CurrentState = GameState.GameOver);
+#endif
 
         GameStateService.StateChanged += GameState_PropertyChanged;
         UpdateCurrentViewModel();
@@ -25,6 +36,12 @@ public class MainWindowViewModel : ObservableObject
     public MenuViewModel MenuVM { get; }
     public GameViewModel GameVM { get; }
     public GameOverViewModel GameOverVM { get; }
+
+#if DEBUG
+    public RelayCommand ToMenuCommand { get; }
+    public RelayCommand ToInGameCommand { get; }
+    public RelayCommand ToGameOverCommand { get; }
+#endif
 
     private ObservableObject? _currentViewModel;
     public ObservableObject? CurrentViewModel
