9c1d2a521e09feaf79f4a27b52e0e9772f849684
Mark6 <grigorash-mark@mail.ru>
2026-03-17 00:32:11 +0700

    add DebuggerDisplay to Cell

--- Match-M/Model/Cell.cs
+++ Match-M/Model/Cell.cs
@@ -1,13 +1,18 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using Match_M.Animations;
+using System.Diagnostics;
 
 namespace Match_M.Model;
+[DebuggerDisplay("Row: {Row} Column: {Column} Shape: {Shape}({(int)Shape}) Bonus: {Bonus} IsSelected: {IsSelected}")]
 public class Cell : ObservableObject
 {
     private ShapeType _shape;
     private bool _isSelected;
     private AnimationType _animation;
     private BonusType _bonus;
+
+    public int Row { get; }
+    public int Column { get; }
 
     public ShapeType Shape
     {
@@ -35,9 +40,6 @@ public class Cell : ObservableObject
         get => _bonus;
         set => SetProperty(ref _bonus, value);
     }
-
-    public int Row { get; }
-    public int Column { get; }
 
     /// <summary>
     /// Расстояние падения в пикселях для анимации (используется поведением при Animation = MoveUpDown).
